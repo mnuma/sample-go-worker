9b616ddf2044bfa9af94fbb311b11e429921820d
mnuma <mh10hiroaki@gmail.com>
2018-09-14 18:33:05 +0900

    init

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"os"
+	"syscall"
+	"os/signal"
+)
+
+func main() {
+	os.Exit(run())
+}
+
+func run() int {
+  ret := loop()
+  return ret
+}
+
+func loop() int {
+
+  log.Println("RUN")
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	code := 0
+	select {
+	case s := <-sigChan:
+		switch s {
+		// kill -SIGHUP XXXX
+		case syscall.SIGHUP:
+			log.Println("SIGHUP")
+		// kill -SIGINT XXXX or Ctrl+c
+		case syscall.SIGINT:
+			log.Println("SIGINT")
+		// kill -SIGTERM XXXX
+		case syscall.SIGTERM:
+			log.Println("SIGTERM")
+		// kill -SIGQUIT XXXX
+		case syscall.SIGQUIT:
+			log.Println("SIGQUIT")
+		default:
+			log.Printf("Unknown signal. %v", s)
+			code = 1
+		}
+	}
+	return code
+}
